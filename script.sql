79da494406b4721574439e250a2032e94452e694
JaimeFc <159406959+JaimeFc@users.noreply.github.com>
2025-10-05 16:03:01 -0500

    Update script.sql

--- script.sql
+++ script.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-09-2025 a las 06:07:58
+-- Tiempo de generación: 05-10-2025 a las 22:57:56
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -24,23 +24,49 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `ID_Categoria` int(11) NOT NULL,
+  `NombreCategoria` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
+INSERT INTO `categorias` (`ID_Categoria`, `NombreCategoria`) VALUES
+(1, 'Bebidas'),
+(2, 'Cafes clasicos'),
+(4, 'Golosinas'),
+(3, 'Reposteria');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `productos`
 --
 
 CREATE TABLE `productos` (
-  `ID_Producto` int(11) NOT NULL COMMENT 'identificador unico',
-  `Nombre` varchar(100) NOT NULL COMMENT 'nombre del producto',
-  `Cantidad` int(100) NOT NULL,
-  `precio` decimal(10,2) DEFAULT NULL
+  `ID_Producto` int(11) NOT NULL,
+  `Nombre` varchar(100) NOT NULL,
+  `Cantidad` int(11) DEFAULT NULL,
+  `precio` decimal(10,2) DEFAULT NULL,
+  `ID_Categoria` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `productos`
 --
 
-INSERT INTO `productos` (`ID_Producto`, `Nombre`, `Cantidad`, `precio`) VALUES
-(1, 'pastel de choco', 5, 15.50),
-(2, 'pastel de vainilla', 2, 12.00);
+INSERT INTO `productos` (`ID_Producto`, `Nombre`, `Cantidad`, `precio`, `ID_Categoria`) VALUES
+(1, 'Avena Polac', 15, 0.75, 1),
+(2, 'Pastel de chocolate ', 5, 10.50, 3),
+(3, 'Pan de Chocolate', 50, 0.20, 3),
+(4, 'Pan De Sal', 30, 0.10, 3),
+(5, 'Coca cola (1lt)', 12, 1.25, 1),
+(6, 'Leche Entera', 24, 0.95, 1);
 
 -- --------------------------------------------------------
 
@@ -49,38 +75,78 @@ INSERT INTO `productos` (`ID_Producto`, `Nombre`, `Cantidad`, `precio`) VALUES
 --
 
 CREATE TABLE `usuarios` (
-  `ID_Usuario` varchar(11) NOT NULL,
-  `Nombre` varchar(100) NOT NULL,
-  `Mail` varchar(225) NOT NULL
+  `idusuario` int(10) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`idusuario`, `nombre`, `email`, `password`) VALUES
+(2147483647, 'jaime', 'jf.castillog@uea.edu.ec', 'scrypt:32768:8:1$hgCciunKSTsjXhqG$b465029046bd2f5c19daf4fa7708d28443d465e17abf4c73a692088f0ae4a0fdc5ca12b58c5c81a7eb0cfb3134f75806f6aaec438b88b3190f51ae37c97a9d89');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`ID_Categoria`),
+  ADD UNIQUE KEY `NombreCategoria` (`NombreCategoria`);
+
+--
 -- Indices de la tabla `productos`
 --
 ALTER TABLE `productos`
-  ADD PRIMARY KEY (`ID_Producto`);
+  ADD PRIMARY KEY (`ID_Producto`),
+  ADD KEY `ID_Categoria` (`ID_Categoria`);
 
 --
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`ID_Usuario`);
+  ADD PRIMARY KEY (`idusuario`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
+-- AUTO_INCREMENT de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `ID_Categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `ID_Producto` int(11) NOT NULL AUTO_INCREMENT COMMENT 'identificador unico', AUTO_INCREMENT=3;
+  MODIFY `ID_Producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `idusuario` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2147483648;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`ID_Categoria`) REFERENCES `categorias` (`ID_Categoria`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
